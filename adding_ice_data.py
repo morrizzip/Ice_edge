ac7ffb3cdf39eaae9a9d21a9383ead9349a192f7
morrizzip <49508606+morrizzip@users.noreply.github.com>
2025-06-25 22:42:14 +0300

    Update adding_ice_data.py

--- adding_ice_data.py
+++ adding_ice_data.py
@@ -5,53 +5,35 @@ res_url = f'{base_url}resource/'
 
 parentID = 218 #id родительской папки
 pointID = 710 #id слоя для точек
-mpID = 711 #id слоя для мультиполигонов
-mlID = 712 #id для мультилиний
+mpID = 2067 #id слоя для мультиполигонов
 
+# функция для получения ID слоя в зависимости от выбранной даты
+def get_mpID_for_date(date):
+    date_mpID_mapping = {
+        "2020feb": 1951,
+        "2021feb": 1961,
+        "2022feb": 1962,
+        "2020mar": 1963,
+        "2021mar": 1964,
+        "2022mar": 1965,
+        "2020jan": 1966,
+        "2021jan": 1967,
+        "2022jan": 1968,
+    }
+    return date_mpID_mapping.get(date, None)
 
+# ввод пользователя
+selected_date = input("Выберите дату (формат YYYY или [YYYYmon]): ")
+
+mpID = get_mpID_for_date(selected_date)
+
+if mpID is not None:
+    print(f"Выбранный ID слоя для мультиполигонов: {mpID}")
+else:
+    print("Неверная дата. Пожалуйста, выберите правильную дату.")
 
 file_point = '.txt'  #координаты для точек
 file_poly = 'ice_edge_coordinates.txt' #координаты для полигонов
-file_line = 'ice_edge_coordinates.txt' #координаты для линий
-
-
-
-#
-# #                                   Создаём группу ресурсов и слои
-#
-
-# response = api.createResourceGroup(res_url,parentID,display_name="POINT",description='folder for point')
-# print(response)
-
-# response = api.createLayer(res_url,parentID,display_name='(release)',description='',geometry_type="",
-#                               fields=[
-#                                   {
-#                                       "keyname": "name",
-#                                       "display_name": "Name",
-#                                       "datatype": "STRING",
-#                                   },
-#                                   {
-#                                       "keyname": "description",
-#                                       "display_name": "Description",
-#                                       "datatype": "STRING",
-#                                   },
-#                               ]
-#                               )
-# print(response.status_code)
-# print(response.json())
-
-
-
-
-#
-#                            добавление данных
-#
-
-# #точки
-# response = api.addPointsFromFile(file_point, res_url, pointID)
-# print(response)
-
-
 
 # добавление мультиполигонов из больших файлов
 k = adding_ice_data_api.split_coordinates_file(file_poly)
@@ -66,22 +48,6 @@ for i in range(k):
 
     response = adding_ice_data_api.addMultiPolygonToVectorLayer(res_url, mpID, result_coords)
     print(f"Result: {file_path_poly}: {response}")
-
-
-
-# # добавление мультилиний из больших файлов
-# k = adding_ice_data_api.split_coordinates_file(file_line)
-#
-# for i in range(k):
-#     file_path_line = f'coordinate_part_{i}.txt'
-#
-#     print(file_path_line)
-#
-#     result_coords = adding_ice_data_api.convert_coordinates(file_path_line)
-#     print(result_coords)
-#
-#     response = adding_ice_data_api.addMLinesToVectorLayer(res_url, mlID, result_coords)
-#     print(f"Result: {file_path_line}: {response}")
 
 
 
